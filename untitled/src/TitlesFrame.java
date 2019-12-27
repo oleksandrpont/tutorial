0081971919c19d9a84e933dfa1bbde9250e76ebe
oleksandrpont <56116860+oleksandrpont@users.noreply.github.com>
2019-12-27 10:35:09 +0200

    Add files via upload
    измененные исходники

---
+++ untitled/src/TitlesFrame.java
@@ -0,0 +1,28 @@
+import javax.swing.SwingUtilities;
+import java.awt.Component;
+import javax.swing.JFrame;
+public class TitlesFrame extends JFrame
+    //78
+{
+    public TitlesFrame() {
+        this.initUI();
+    }
+
+    private void initUI() {
+        this.setTitle("\u041a\u0440\u0438\u0432\u044b\u0435 \u0444\u0438\u0433\u0443\u0440\u044b");
+        this.setDefaultCloseOperation(3);
+        this.add(new TitlesPanel(18));
+        this.setSize(350, 350);
+        this.setLocationRelativeTo(null);
+    }
+
+    public static void main(final String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                final TitlesFrame ps = new TitlesFrame();
+                ps.setVisible(true);
+            }
+        });
+    }
+}
